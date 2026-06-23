6b23896a286f02de15900b912e8efdf5f6d112c5
Ruchika3015 <chaudharyruchika206@gmail.com>
2026-06-23 18:49:17 +0530

    Day 2 done

---
+++ Week 4/Day 2/binary_tree_zigzag.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==nullptr)
+        return ans;
+        bool lefttoright=true;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n=q.size();
+            vector<int>level(n);
+            for(int i=0;i<n;i++)
+            {
+                TreeNode*temp=q.front();
+                q.pop();
+                int idx=lefttoright?i:n-i-1;
+                level[idx]=temp->val;
+                if(temp->left)
+                q.push(temp->left);
+                if(temp->right)
+                q.push(temp->right);
+            }
+            lefttoright=!lefttoright;
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
